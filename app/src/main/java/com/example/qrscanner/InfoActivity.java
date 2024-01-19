59639fa316e370fc9ef328ee99d5333380b11aa8
Henrik <91131175+hen47rik@users.noreply.github.com>
2024-01-19 14:16:32 +0100

    added HomeButton to InfoActivity
    HomeButton to return to MainActivity form InfoActivity

--- app/src/main/java/com/example/qrscanner/InfoActivity.java
+++ app/src/main/java/com/example/qrscanner/InfoActivity.java
@@ -2,15 +2,26 @@ package com.example.qrscanner;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 
 public class InfoActivity extends Activity {
-    Button homebutton;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        button = findViewById(R.id.homeButton);
 
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
